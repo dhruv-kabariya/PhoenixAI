a87ca1ee83ea8abf06938a3e8d8ee13ca5288420
Abhishek <abhishekghaskata1999@gmail.com>
2020-07-07 11:55:06 +0530

    change login route

--- lib/signup/ui/signupscreen.dart
+++ lib/signup/ui/signupscreen.dart
@@ -1,6 +1,5 @@
 import 'package:ai/signup/ui/confirmSignUp.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SignUpScreen extends StatelessWidget {
   @override
